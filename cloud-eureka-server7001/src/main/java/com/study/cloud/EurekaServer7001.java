ee0d2498c24a3b7fe2ad1127302ff3fcb9214075
yangtongkuan <yangtongkuan@qq.com>
2020-03-16 13:59:02 +0800

    # eureka 集群搭建

--- cloud-eureka-server7001/src/main/java/com/study/cloud/EurekaServer7001.java
+++ cloud-eureka-server7001/src/main/java/com/study/cloud/EurekaServer7001.java
@@ -2,13 +2,12 @@ package com.study.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaClient
-public class CloudConsumerOrder80 {
-
+@EnableEurekaServer
+public class EurekaServer7001 {
     public static void main(String[] args) {
-        SpringApplication.run(CloudConsumerOrder80.class, args);
+        SpringApplication.run(EurekaServer7001.class, args);
     }
 }
--- cloud-provider-payment8001/src/main/java/com/study/cloud/PaymentMain8001.java
+++ cloud-provider-payment8001/src/main/java/com/study/cloud/PaymentMain8001.java
@@ -2,26 +2,12 @@ package com.study.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.io.IOException;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@RestController
+@EnableEurekaClient
 public class PaymentMain8001 {
     public static void main(String[] args) {
         SpringApplication.run(PaymentMain8001.class, args);
-    }
-
-    @GetMapping("/hello")
-    public String hello() {
-        String cmd = "./usr/local/jars/test/start.sh restart";
-        try {
-            Runtime.getRuntime().exec(cmd);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return "hello word";
     }
 }
